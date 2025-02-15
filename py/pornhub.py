1d473274e5d09094f70ac71229e89b1ad12e68d2
Nobody <185114932@qq.com>
2025-02-15 23:19:34 +0800

    Update pornhub.py

--- py/pornhub.py
+++ py/pornhub.py
@@ -27,7 +27,7 @@ class Spider(Spider):
     def destroy(self):
         pass
 
-    host = "http://127.0.0.1:10079/p/0/127.0.0.1:10172/https://www.pornhub.com"
+    host = "https://cn.pornhub.com"
 
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5410.0 Safari/537.36',
