e31c6c4822854e536d286c00f2c99bdea4b425b5
Nobody <185114932@qq.com>
2025-02-26 07:27:03 +0800

    Update py_Xhm.py

--- py/py_Xhm.py
+++ py/py_Xhm.py
@@ -18,11 +18,10 @@ class Spider(Spider):
         self.session = Session()
         self.session.headers.update(self.headers)
         # 设置代理
-        self.proxies = {}
-        # self.proxies = {
-        #     'http': 'http://127.0.0.1:10172',
-        #     'https': 'http://127.0.0.1:10172'
-        # }
+        self.proxies = {
+            'http': 'http://127.0.0.1:10172',
+            'https': 'http://127.0.0.1:10172'
+        }
         if self.is_port_open('127.0.0.1', 7891):
             self.proxies = {
                 'http': 'http://127.0.0.1:7891',
