0bf0a91b0851673efc88b83b7947581eaacd5537
Nobody <185114932@qq.com>
2025-03-01 08:59:24 +0800

    Update py_色播聚合.py

--- py/py_色播聚合.py
+++ py/py_色播聚合.py
@@ -21,8 +21,7 @@ class Spider(Spider):
 				"vod_id":"/"+item['address'],
 				"vod_name": item['title'],
 				"vod_pic": item['xinimg'].replace("http://cdn.gcufbd.top/img/", "https://slink.ltd/https://raw.githubusercontent.com/fish2018/lib/refs/heads/main/imgs/"),
-				# "vod_remarks": item['Number'],
-				"vod_remarks": 120,
+				"vod_remarks": item['Number'],
 				"style": {"type": "rect", "ratio": 1.33}
 			} for item in data]
 		result = {
